94741fa85eff86821e49a9953117b3087b48f1f4
Jasmine Buckley <jasmine.buckley@softwire.com>
2020-09-08 17:02:45 +0100

    Fizz to Bong added

--- app/src/main/java/com/example/fizzbuzzns/FizzBuzz.kt
+++ app/src/main/java/com/example/fizzbuzzns/FizzBuzz.kt
@@ -1,5 +1,30 @@
 package com.example.fizzbuzzns
 
 fun main(){
-    println("Hello world!")
+    fizzBuzz0To100()
 }
+
+fun fizzBuzz0To100(){
+    for (i in 0..100){
+        if (multipleOf(i,11)){
+            print("Bong")
+        }
+        else {
+            if (multipleOf(i, 3)) {
+                print("Fizz")
+            }
+            if (multipleOf(i, 5)) {
+                print("Buzz")
+            }
+            if (multipleOf(i, 7)) {
+                print("Bang")
+            }
+            if (!multipleOf(i, 3) && !multipleOf(i, 5)) {
+                print(i)
+            }
+        }
+        println()
+    }
+}
+
+fun multipleOf(toCheck: Int, multiple: Int) = toCheck%multiple==0
