d588bc5d0e687346cfd454d021c69057c135dc6c
Jasmine Buckley <jasmine.buckley@softwire.com>
2020-09-09 11:00:51 +0100

    Refactored using joinToString method and added error handling to user input

--- app/src/main/java/com/example/fizzbuzzns/FizzBuzz.kt
+++ app/src/main/java/com/example/fizzbuzzns/FizzBuzz.kt
@@ -1,5 +1,8 @@
 package com.example.fizzbuzzns
 
+import java.lang.Exception
+import kotlin.properties.Delegates
+
 fun main(){
     val max = getMax()
     val outputs = generateFizzBuzz0To(max)
@@ -8,7 +11,18 @@ fun main(){
 
 fun getMax(): Int{
     println("Please enter the list maximum: ")
-    return Integer.valueOf(readLine())
+    var userInputTaken = false
+    var max by Delegates.notNull<Int>()
+    while (!userInputTaken){
+        try {
+            max = Integer.valueOf(readLine())
+            userInputTaken = true
+        }
+        catch(e: Exception){
+            println("Please try again, ensuring you have entered a WHOLE NUMBER to be taken as the maximum value: ")
+        }
+    }
+    return max
 }
 
 fun generateFizzBuzz0To(max: Int): MutableList<String>{
@@ -41,21 +55,12 @@ fun generateFizzBuzz0To(max: Int): MutableList<String>{
         if (multipleOf(i,17)){
             output.reverse()
         }
-        val outputString = concatenateOutputIntoString(output)
-        outputs.add(outputString)
+        outputs.add(output.joinToString(separator = ""))
     }
     return outputs
 }
 
 fun multipleOf(toCheck: Int, multiple: Int) = toCheck%multiple==0
-
-fun concatenateOutputIntoString(output: MutableList<String>): String{
-    var outputString = ""
-    for (word in output){
-        outputString += word
-    }
-    return outputString
-}
 
 fun printOutputs(outputs: MutableList<String>){
     for (output in outputs){
